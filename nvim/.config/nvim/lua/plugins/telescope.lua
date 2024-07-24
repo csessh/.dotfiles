4787c93e5a17072e2157d243e11bdda5c3a8bf14
Thang Do <thang.m.do@sony.com>
2024-07-24 19:50:39 +0930

    Added lualine

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,14 +1,12 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        tag='0.1.8', 
-        dependencies={
-            'nvim-lua/plenary.nvim'
-        },
-        config = function()
-            local telescope = require('telescope.builtin')
-            vim.keymap.set('n', '<C-f>', telescope.find_files, {})
-            vim.keymap.set('n', '<leader>grep', telescope.live_grep, {})
-        end
-    }
+    'nvim-telescope/telescope.nvim',
+    tag='0.1.8', 
+    dependencies={
+        'nvim-lua/plenary.nvim'
+    },
+    config = function()
+        local telescope = require('telescope.builtin')
+        vim.keymap.set('n', '<C-f>', telescope.find_files, {})
+        vim.keymap.set('n', '<leader>grep', telescope.live_grep, {})
+    end
 }
