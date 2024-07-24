92127fa69049bf570baeca3ab67fbdacbb1ee98c
Thang Do <thang.m.do@sony.com>
2024-07-24 22:29:31 +0930

    Modify neotree to show dotfiles

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,3 +1,5 @@
+vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>', {})
+
 return {
     'nvim-neo-tree/neo-tree.nvim', 
     branch = 'v3.x', 
@@ -7,6 +9,39 @@ return {
         'MunifTanjim/nui.nvim'
     },
     config = function()
-        vim.keymap.set('n', '<leader>b', ':Neotree toggle<CR>', {})
+        require('neo-tree').setup({
+            close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+            popup_border_style = "rounded",
+            enable_git_status = true,
+            enable_diagnostics = true,
+            sort_case_insensitive = true, -- used when sorting files and directories in the tree
+            filesystem = {
+                filtered_items = {
+                    visible = true, -- when true, they will just be displayed differently than normal items
+                    hide_dotfiles = true,
+                    hide_gitignored = false
+                }
+            },
+            default_component_configs = {
+                name = {
+                    trailing_slash = false,
+                    use_git_status_colors = true,
+                    highlight = "NeoTreeFileName"
+                },
+                git_status = {
+                    symbols = {
+                        added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+                        modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+                        deleted   = "✖",-- this can only be used in the git_status source
+                        renamed   = "󰁕",-- this can only be used in the git_status source
+                        untracked = "",
+                        ignored   = "",
+                        unstaged  = "󰄱",
+                        staged    = "",
+                        conflict  = ""
+                    }
+                }
+            }
+        })
     end
 }
