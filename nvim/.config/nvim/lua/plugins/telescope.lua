74cac78f599ccb920ba52641c0e9fa123fcc3f2d
Thang Do <thang.m.do@sony.com>
2024-07-24 22:42:13 +0930

    Updated telescope: theme and keymaps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,7 +6,26 @@ return {
     },
     config = function()
         local telescope = require('telescope.builtin')
-        vim.keymap.set('n', '<C-f>', telescope.find_files, {})
-        vim.keymap.set('n', '<leader>grep', telescope.live_grep, {})
+        vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
+        vim.keymap.set('n', '<leader>lg', telescope.live_grep, {})
+        vim.keymap.set('n', '<leader>gs', telescope.grep_string, {})
+
+        require('telescope').setup({
+            defaults = {
+            },
+            pickers = {
+                find_files = {
+                    theme = "dropdown"
+                },
+                live_grep =  {
+                    theme = "dropdown"
+                },
+                grep_string = {
+                    theme = "dropdown"
+                },
+            },
+            extensions = {
+            }
+        })
     end
 }
