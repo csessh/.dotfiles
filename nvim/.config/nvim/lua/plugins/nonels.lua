53a5c786caf4ddc126ac6d9d869249074af46ec8
Thang Do <thang.m.do@sony.com>
2024-07-26 23:06:51 +0930

    Bash linter and code action

--- nvim/.config/nvim/lua/plugins/nonels.lua
+++ nvim/.config/nvim/lua/plugins/nonels.lua
@@ -1,21 +1,29 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- Lua
-				null_ls.builtins.formatting.stylua,
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+        "gbprod/none-ls-shellcheck.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                -- Lua
+                null_ls.builtins.formatting.stylua,
 
-				-- Python
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.diagnostics.mypy,
-				null_ls.builtins.diagnostics.ruff,
+                -- Python
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.diagnostics.mypy,
+                require("none-ls.diagnostics.flake8"),
 
-				-- Docker
-				null_ls.builtins.diagnostics.hadolint,
-			},
-		})
-	end,
+                -- Bash
+                require("none-ls-shellcheck.diagnostics"),
+                require("none-ls-shellcheck.code_actions"),
+
+                -- Docker
+                null_ls.builtins.diagnostics.hadolint,
+            },
+        })
+    end,
 }
