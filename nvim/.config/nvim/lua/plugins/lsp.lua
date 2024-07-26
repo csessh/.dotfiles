fc478035510e874f64116d1cc56251f9879c7258
Thang Do <thang.m.do@sony.com>
2024-07-26 14:12:16 +0930

    Python linter + formatter

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,84 +1,84 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"clangd",
-				"bashls",
-				"dockerls",
-				"docker_compose_language_service",
-				--'gopls',
-				--'graphql',
-				"jsonls",
-				--'java_language_server',
-				"markdown_oxide",
-				"pyright",
-				"sqlls",
-				"taplo",
-				"yamlls",
-				"helm_ls",
-			},
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.clangd.setup({})
-			lspconfig.bashls.setup({})
-			lspconfig.dockerls.setup({})
-			lspconfig.docker_compose_language_service.setup({})
-			lspconfig.jsonls.setup({})
-			lspconfig.markdown_oxide.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.sqlls.setup({})
-			lspconfig.taplo.setup({})
-			lspconfig.yamlls.setup({})
-			lspconfig.helm_ls.setup({})
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "lua_ls",
+                "clangd",
+                "bashls",
+                "dockerls",
+                "docker_compose_language_service",
+                --'gopls',
+                --'graphql',
+                "jsonls",
+                --'java_language_server',
+                "markdown_oxide",
+                "pyright",
+                "sqlls",
+                "taplo",
+                "yamlls",
+                "helm_ls",
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+            lspconfig.clangd.setup({})
+            lspconfig.bashls.setup({})
+            lspconfig.dockerls.setup({})
+            lspconfig.docker_compose_language_service.setup({})
+            lspconfig.jsonls.setup({})
+            lspconfig.markdown_oxide.setup({})
+            lspconfig.pyright.setup({})
+            lspconfig.sqlls.setup({})
+            lspconfig.taplo.setup({})
+            lspconfig.yamlls.setup({})
+            lspconfig.helm_ls.setup({})
 
-			-- Global mappings.
-			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
-			vim.keymap.set("n", "[", vim.diagnostic.goto_prev)
-			vim.keymap.set("n", "]", vim.diagnostic.goto_next)
+            -- Global mappings.
+            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
+            vim.keymap.set("n", "[", vim.diagnostic.goto_prev)
+            vim.keymap.set("n", "]", vim.diagnostic.goto_next)
 
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+            -- Use LspAttach autocommand to only map the following keys
+            -- after the language server attaches to the current buffer
+            vim.api.nvim_create_autocmd("LspAttach", {
+                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+                callback = function(ev)
+                    -- Enable completion triggered by <c-x><c-o>
+                    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { buffer = ev.buf }
-					vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references, opts)
-					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set("n", "?", vim.lsp.buf.hover, opts)
+                    -- Buffer local mappings.
+                    -- See `:help vim.lsp.*` for documentation on any of the below functions
+                    local opts = { buffer = ev.buf }
+                    vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts)
+                    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts)
+                    vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references, opts)
+                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "?", vim.lsp.buf.hover, opts)
 
-					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-					vim.keymap.set("n", "<leader>wl", function()
-						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					end, opts)
+                    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+                    vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                    vim.keymap.set("n", "<leader>wl", function()
+                        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                    end, opts)
 
-					vim.keymap.set("n", "<leader>fm", function()
-						vim.lsp.buf.format({ async = true })
-					end, opts)
-				end,
-			})
-		end,
-	},
+                    vim.keymap.set("n", "<leader>fm", function()
+                        vim.lsp.buf.format({ async = true })
+                    end, opts)
+                end,
+            })
+        end,
+    },
 }
--- nvim/.config/nvim/lua/plugins/nonels.lua
+++ nvim/.config/nvim/lua/plugins/nonels.lua
@@ -4,7 +4,14 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
+				-- Lua
 				null_ls.builtins.formatting.stylua,
+
+				-- Python
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.isort,
+				null_ls.builtins.diagnostics.mypy,
+				null_ls.builtins.diagnostics.ruff,
 			},
 		})
 	end,
