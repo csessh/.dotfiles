d568b2d4814b73f111a32b01761aa8967e2f245d
Thang Do <csessh@hey.com>
2024-07-30 13:56:22 +0930

    ALL CAPS just got easier

--- nvim/.config/nvim/lua/plugins/nonels.lua
+++ nvim/.config/nvim/lua/plugins/nonels.lua
@@ -15,7 +15,7 @@ return {
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
                 null_ls.builtins.diagnostics.mypy,
-                require "none-ls.diagnostics.flake8",
+                null_ls.builtins.diagnostics.pylint,
 
                 -- Bash
                 require "none-ls-shellcheck.diagnostics",
