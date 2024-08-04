e27f283774c3fbb47d5715ab129a125cc5d18353
Thang Do <csessh@hey.com>
2024-08-04 22:44:20 +0930

    Add theme and set a default option = Eldritch

--- nvim/.config/nvim/lua/plugins/scheme.lua
+++ nvim/.config/nvim/lua/plugins/scheme.lua
@@ -3,17 +3,25 @@ return {
         "zaldih/themery.nvim",
         opts = {
             livePreview = true,
-            themes = { "eldritch", "dracula" },
+            themes = { "eldritch", "dracula", "nordic" },
         },
     },
     {
         "eldritch-theme/eldritch.nvim",
         lazy = false,
         name = "eldritch",
+        config = function()
+            require 'eldritch'.load()
+        end
     },
     {
         "Mofiqul/dracula.nvim",
         lazy = false,
         name = "dracula",
     },
+    {
+        'AlexvZyl/nordic.nvim',
+        lazy = false,
+        name = "nordic",
+    }
 }
