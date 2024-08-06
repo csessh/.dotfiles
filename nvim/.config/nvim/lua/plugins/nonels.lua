b2d3bf3a089f864462eafad9a1ffd2d9f95b6b1f
Thang Do <csessh@hey.com>
2024-08-06 14:45:21 +0930

    Remove pylint

--- nvim/.config/nvim/lua/plugins/nonels.lua
+++ nvim/.config/nvim/lua/plugins/nonels.lua
@@ -15,7 +15,6 @@ return {
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.formatting.isort,
                 null_ls.builtins.diagnostics.mypy,
-                null_ls.builtins.diagnostics.pylint,
 
                 -- Bash
                 require "none-ls-shellcheck.diagnostics",
