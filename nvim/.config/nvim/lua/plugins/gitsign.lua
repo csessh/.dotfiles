98bfef45b786b46301fd275abee3fe5744c8df5a
Thang Do <csessh@hey.com>
2024-08-10 13:07:57 +0930

    Gitsigns configs to highlight changes better visually

--- nvim/.config/nvim/lua/plugins/gitsign.lua
+++ nvim/.config/nvim/lua/plugins/gitsign.lua
@@ -1,6 +1,12 @@
 return {
     "lewis6991/gitsigns.nvim",
-    config = function ()
-        require('gitsigns').setup()
-    end
+    config = function()
+        require("gitsigns").setup {
+            numhl = true,
+            linehl = false,
+            signcolumn = true,
+            current_line_blame = true,
+            current_line_blame_formatter = "<author> <author_time:%R>: <summary>",
+        }
+    end,
 }
