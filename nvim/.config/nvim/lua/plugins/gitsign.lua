1441a3d541a16f4120eaf59669a41230a66b105d
Thang Do <csessh@hey.com>
2024-08-12 21:27:01 +0930

    gitsign action simplified

--- nvim/.config/nvim/lua/plugins/gitsign.lua
+++ nvim/.config/nvim/lua/plugins/gitsign.lua
@@ -35,29 +35,17 @@ return {
 
                 -- Actions
                 map("n", "<leader>sh", gitsigns.stage_hunk)
-                map("n", "<leader>rh", gitsigns.reset_hunk)
                 map("v", "<leader>sh", function()
                     gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
                 end)
+
+                map("n", "<leader>rh", gitsigns.reset_hunk)
                 map("v", "<leader>rh", function()
                     gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
                 end)
-                map("n", "<leader>hS", gitsigns.stage_buffer)
-                map("n", "<leader>uh", gitsigns.undo_stage_hunk)
-                map("n", "<leader>hR", gitsigns.reset_buffer)
-                map("n", "<leader>ph", gitsigns.preview_hunk)
-                map("n", "<leader>bl", function()
-                    gitsigns.blame_line { full = true }
-                end)
-                map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
-                map("n", "<leader>d", gitsigns.diffthis)
-                map("n", "<leader>D", function()
-                    gitsigns.diffthis "~"
-                end)
-                map("n", "<leader>td", gitsigns.toggle_deleted)
 
-                -- Text object
-                map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+                map("n", "<leader>uh", gitsigns.undo_stage_hunk)
+                map("n", "<leader>ph", gitsigns.preview_hunk)
             end,
         }
     end,
