4e77ec95ae3f34238b674c2b8a0408b66359dea1
Thang Do <csessh@hey.com>
2024-08-14 22:41:23 +0930

    Added a couple more colorschemes

--- nvim/.config/nvim/lua/plugins/scheme.lua
+++ nvim/.config/nvim/lua/plugins/scheme.lua
@@ -3,25 +3,39 @@ return {
         "zaldih/themery.nvim",
         opts = {
             livePreview = true,
-            themes = { "eldritch", "dracula", "nordic" },
+            themes = { "eldritch", "everforest", "dracula", "nordic", "cyberdream" },
         },
     },
     {
         "eldritch-theme/eldritch.nvim",
         lazy = false,
         name = "eldritch",
-        config = function()
-            require 'eldritch'.load()
-        end
     },
     {
         "Mofiqul/dracula.nvim",
         lazy = false,
         name = "dracula",
+        priority = 1000, -- make sure to load this before all the other start plugins
     },
     {
-        'AlexvZyl/nordic.nvim',
+        "AlexvZyl/nordic.nvim",
         lazy = false,
         name = "nordic",
-    }
+        priority = 1000, -- make sure to load this before all the other start plugins
+    },
+    {
+        "scottmckendry/cyberdream.nvim",
+        lazy = false,
+        name = "cyberdrem",
+        priority = 1000, -- make sure to load this before all the other start plugins
+    },
+    {
+        "neanias/everforest-nvim",
+        version = false,
+        lazy = false,
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require("everforest").load()
+        end,
+    },
 }
