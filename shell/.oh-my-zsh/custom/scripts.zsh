3962cb0770dbf9dd29109ac99193742ca13f1b3d
Thang Do <csessh@hey.com>
2024-09-14 20:06:32 +0930

    Reorganise zshrc

---
+++ shell/.oh-my-zsh/custom/scripts.zsh
@@ -0,0 +1,26 @@
+####################################################################
+#  FUNCTIONS 
+####################################################################
+
+sniper() {
+    ps aux | fzf --height=40% --layout=reverse --prompt="Which process are we killing? > " | awk '{print $2}' | xargs -r sudo kill
+}
+
+nah() {
+    git reset --hard
+
+    if [[ $1 == "-f" ]]; then
+        git clean -df
+    fi
+}
+
+activate() {
+    if [ -z "$1" ]; then
+        echo "virtualenv name is required."
+        echo "Select one of the following to source:"
+        ls ~/Documents/pyenvs 
+    else
+        source ~/Documents/pyenvs/$1/bin/activate
+    fi
+}
+
