9e0111ba9713a961b682edae1266a57d202e0613
Thang Do <csessh@hey.com>
2024-09-14 17:10:02 +0930

    More leap.nvim tweaks

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -3,18 +3,12 @@ return {
     config = function()
         vim.keymap.set("n", "s", "<Plug>(leap)")
         vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+        vim.keymap.set({ "x", "o" }, "x", "<Plug>(leap-forward-till)")
+        vim.keymap.set({ "x", "o" }, "X", "<Plug>(leap-backward-till)")
         vim.keymap.set({ "x", "o" }, "s", "<Plug>(leap-forward)")
         vim.keymap.set({ "x", "o" }, "S", "<Plug>(leap-backward)")
 
-        vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" }) -- or some grey
-        vim.api.nvim_set_hl(0, "LeapMatch", {
-            -- For light themes, set to 'black' or similar.
-            fg = "white",
-            bold = true,
-            nocombine = true,
-        })
-
         -- Disable auto-jumping to the first match
-        -- require("leap").opts.safe_labels = {}
+        require("leap").opts.safe_labels = {}
     end,
 }
