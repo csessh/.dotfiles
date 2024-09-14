21504c65b966cccf7b86eaa38230dd386dab125c
Thang Do <csessh@hey.com>
2024-09-14 16:53:19 +0930

    leap.nvim: tweaks to optimise visual aids

--- nvim/.config/nvim/lua/plugins/leap.lua
+++ nvim/.config/nvim/lua/plugins/leap.lua
@@ -1,11 +1,18 @@
 return {
     "ggandor/leap.nvim",
     config = function()
-        vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward-to)")
-        vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward-to)")
-        vim.keymap.set({ "n", "x", "o" }, "x", "<Plug>(leap-forward-till)")
-        vim.keymap.set({ "n", "x", "o" }, "X", "<Plug>(leap-backward-till)")
-        vim.keymap.set({ "n", "x", "o" }, "gs", "<Plug>(leap-from-window)")
+        vim.keymap.set("n", "s", "<Plug>(leap)")
+        vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+        vim.keymap.set({ "x", "o" }, "s", "<Plug>(leap-forward)")
+        vim.keymap.set({ "x", "o" }, "S", "<Plug>(leap-backward)")
+
+        vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" }) -- or some grey
+        vim.api.nvim_set_hl(0, "LeapMatch", {
+            -- For light themes, set to 'black' or similar.
+            fg = "white",
+            bold = true,
+            nocombine = true,
+        })
 
         -- Disable auto-jumping to the first match
         -- require("leap").opts.safe_labels = {}
