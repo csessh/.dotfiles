ac1cee963c0a0435c02c39c7adc8af41b60dd7ee
Thang Do <csessh@hey.com>
2024-09-14 21:23:53 +0930

    Added git conflict resolution plugin

---
+++ nvim/.config/nvim/lua/plugins/git-conflict.lua
@@ -0,0 +1,14 @@
+return {
+    "akinsho/git-conflict.nvim",
+    version = "*",
+    opts = {
+        default_mappings = {
+            ours = "o",
+            theirs = "t",
+            none = "0",
+            both = "b",
+            next = "n",
+            prev = "p",
+        },
+    },
+}
