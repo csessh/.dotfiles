025f55a4c6e65ef1614e5381bb8638b8777c57fb
Thang Do <csessh@hey.com>
2024-09-30 13:45:41 +0930

    Removed gitsigns action keymap in favour of interactive gui lazygit for smoother workflow
    All gitsigns keymaps removed. visual aid configs are still present.

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -7,46 +7,6 @@ return {
             signcolumn = true,
             current_line_blame = true,
             current_line_blame_formatter = "<author> (<author_time:%R>): <summary>",
-
-            on_attach = function(bufnr)
-                local gitsigns = require "gitsigns"
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
-
-                -- Navigation
-                map("n", ",h", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal { "]c", bang = true }
-                    else
-                        gitsigns.nav_hunk "next"
-                    end
-                end)
-
-                map("n", ",H", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal { "[c", bang = true }
-                    else
-                        gitsigns.nav_hunk "prev"
-                    end
-                end)
-
-                -- Actions
-                map("n", ",s", gitsigns.stage_hunk)
-                map("v", ",s", function()
-                    gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
-                end)
-                map("n", ",S", gitsigns.undo_stage_hunk)
-
-                map("n", ",r", gitsigns.reset_hunk)
-                map("v", ",r", function()
-                    gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
-                end)
-
-                map("n", ",p", gitsigns.preview_hunk)
-            end,
         }
     end,
 }
