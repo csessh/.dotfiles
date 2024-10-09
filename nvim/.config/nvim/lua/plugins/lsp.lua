f81f2afb667eee44e2d58a144805edc002d1b8b0
Thang Do <csessh@hey.com>
2024-10-09 14:54:04 +1030

    Bring back lsp rename

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -54,6 +54,7 @@ return {
                     local opts = { buffer = ev.buf }
                     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
                     vim.keymap.set("n", "g?", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
                     vim.keymap.set("n", "<leader>fm", function()
                         vim.lsp.buf.format { async = false }
