f3916c7833a9ddf9c27a4e212b1d445a2bacb59b
Thang Do <csessh@hey.com>
2024-10-09 13:02:17 +1030

    Disable diagnostic CursorLine highlight
    tiny-inline-diagnostic.nvim CursorLine highlight clashes with leap.nvim
    search mode

--- nvim/.config/nvim/lua/plugins/diagnostic.lua
+++ nvim/.config/nvim/lua/plugins/diagnostic.lua
@@ -2,8 +2,15 @@ return {
     "rachartier/tiny-inline-diagnostic.nvim",
     event = "LspAttach",
     opts = {
+        hi = { background = "None" },
         options = {
             show_source = true,
+            multiple_diag_under_cursor = true,
         },
+		severity = {
+			vim.diagnostic.severity.ERROR,
+			vim.diagnostic.severity.WARN,
+			vim.diagnostic.severity.INFO,
+		},
     },
 }
