8f4ffc9e579d5e34a4b18e38fe25b8c06f2f50ce
Thang Do <csessh@hey.com>
2024-10-28 14:38:03 +1030

    Remap splitjoin.nvim

--- nvim/.config/nvim/lua/plugins/splitjoin.lua
+++ nvim/.config/nvim/lua/plugins/splitjoin.lua
@@ -2,7 +2,7 @@ return {
     'bennypowers/splitjoin.nvim',
     lazy = true,
     keys = {
-        { 'gj', function() require 'splitjoin'.join() end,  { noremap = true, desc = 'Join the object under cursor' } },
-        { 'gJ', function() require 'splitjoin'.split() end, { noremap = true, desc = 'Split the object under cursor' } },
+        { 'gJ', function() require 'splitjoin'.join() end,  { noremap = true, desc = 'Join the object under cursor' } },
+        { 'gj', function() require 'splitjoin'.split() end, { noremap = true, desc = 'Split the object under cursor' } },
     },
 }
