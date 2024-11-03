62295507acb32f725e0310de7f2ae090a2d2c9bc
Thang Do <csessh@hey.com>
2024-11-03 22:01:51 +1030

    hlargs config updated

--- nvim/.config/nvim/lua/plugins/hlargs.lua
+++ nvim/.config/nvim/lua/plugins/hlargs.lua
@@ -1,11 +1,11 @@
 return {
     "m-demare/hlargs.nvim",
-    lazy = true,
-    event = "BufEnter",
     opts = {
         color = "#f2e422",
         excluded_argnames = {
-            declarations = { "self", "cls" },
+            declarations = {
+                python = { "self", "cls" },
+            },
             usages = {
                 python = { "self", "cls" },
                 lua = { "self" },
