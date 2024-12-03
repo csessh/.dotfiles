0b810cbb88a968e190a0e47faae5d37fc504af0d
Thang Do <csessh@hey.com>
2024-12-03 14:30:25 +1030

    Double tap is annoying. Remap some of the keymaps using double tap

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -9,19 +9,19 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>j", "o<Esc>", { desc = "Add a blank line below current line. Stay in Normal mode" })
 vim.keymap.set("n", "<leader>k", "O<Esc>", { desc = "Add a blank line above current line. Stay in Normal mode" })
 vim.keymap.set("n", "<leader><Esc>", ":noh<CR>", { desc = "Clear search highlights" })
-vim.keymap.set("n", "<Tab>", "<C-w>", { noremap = true, desc = "Shorthand for <C-w>" })
+vim.keymap.set("n", "<Tab>", "<C-w>", { desc = "Shorthand for <C-w>" })
 
 -- Use <leader>p to paste after vi[wWp..]
 -- Think "Select and replace" operation -> <leader>p
-vim.keymap.set("x", "<leader>p", '"_dP', { desc = 'Paste without losing "" register value' })
+vim.keymap.set("x", "<leader>p", '"_dP', { desc = 'Paste, while in visual mode, without losing "" register value' })
 vim.keymap.set("x", "/", "<Esc>/\\%V", { noremap = true, desc = "Search forward within visually selected texts" })
 vim.keymap.set("x", "?", "<Esc>?\\%V", { noremap = true, desc = "Search backward within visually selected texts" })
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit Insert mode and return to Terminal mode" })
 vim.keymap.set("n", "<C-`>", ":tabnew<CR>:terminal<CR>", { desc = "Open Terminal in new tab" })
 
-vim.keymap.set("n", "<leader><leader>x", ":source %<CR>", { desc = "Source current file" })
-vim.keymap.set("n", "<leader><leader>l", ":Lazy<CR>", { desc = "Open LazyVim" })
-vim.keymap.set("n", "<leader><leader>m", ":Mason<CR>", { desc = "Open Mason" })
+vim.keymap.set("n", "<leader>X", ":source %<CR>", { desc = "Source current file" })
+vim.keymap.set("n", "<leader>L", ":Lazy<CR>", { desc = "Open LazyVim" })
+vim.keymap.set("n", "<leader>M", ":Mason<CR>", { desc = "Open Mason" })
 
 vim.keymap.set("n", "Q", "<nop>", { desc = "Disabled" })
 vim.keymap.set("n", "<Tab><Tab>", "<nop>", { desc = "Disabled" })
