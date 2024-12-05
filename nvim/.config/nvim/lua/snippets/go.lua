7e89f770d22baa5be334340d0afff6bb247ff1ed
Thang Do <csessh@hey.com>
2024-12-05 13:31:43 +1030

    Golang snippets

---
+++ nvim/.config/nvim/lua/snippets/go.lua
@@ -0,0 +1,11 @@
+require("luasnip.session.snippet_collection").clear_snippets "go"
+
+local ls = require "luasnip"
+local s = ls.snippet
+local i = ls.insert_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+ls.add_snippets("go", {
+    s("printf", fmt('fmt.Printf("{} %d", {})', { i(1), i(2) })),
+    s("!err", fmt("if err != nil\n{{\n\t{}\n}}\n\n{}", { i(1), i(2) })),
+})
