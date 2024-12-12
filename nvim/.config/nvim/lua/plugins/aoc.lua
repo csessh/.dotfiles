37c3ba3e6a876386fa1de4cd60fabfa88381248d
Thang Do <csessh@hey.com>
2024-12-12 19:37:40 +1030

    Lazy sync

--- nvim/.config/nvim/lua/plugins/aoc.lua
+++ nvim/.config/nvim/lua/plugins/aoc.lua
@@ -1,5 +1,7 @@
 return {
     "csessh/aoc.nvim",
+    -- dir = "~/Documents/nvim-plugins/aoc.nvim/",
+    -- dev = true,
     opts = {
         session_filepath = "/var/tmp/aoc/session.txt",
         puzzle_input = {
