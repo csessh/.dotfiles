c97290e9234a9c2d599c5b184a03f68e50bc7b53
Thang Do <csessh@hey.com>
2025-02-14 22:21:58 +1030

    Misc: Rename plugin file

--- nvim/.config/nvim/lua/plugins/tmux-nvim-navigation.lua
+++ nvim/.config/nvim/lua/plugins/tmux-nvim-navigation.lua
@@ -1,7 +1,6 @@
 return {
     'alexghergh/nvim-tmux-navigation',
     opts = {
-        disable_when_zoomed = true, -- defaults to false
         keybindings = {
             left = "<C-h>",
             down = "<C-j>",
