95f8a6c0ca3483f2b94dea44f4c3c11130dc0649
Thang Do <csessh@hey.com>
2025-03-11 10:00:48 +0700

    Remove inconsistent lsplines plugin

--- nvim/.config/nvim/lua/plugins/linters.lua
+++ nvim/.config/nvim/lua/plugins/linters.lua
@@ -13,13 +13,4 @@ return {
             }
         end,
     },
-    {
-        "maan2003/lsp_lines.nvim",
-        lazy = false,
-        config = function()
-            require("lsp_lines").setup()
-            vim.diagnostic.config { virtual_text = false }
-            vim.diagnostic.config { virtual_lines = { only_current_line = true } }
-        end,
-    },
 }
