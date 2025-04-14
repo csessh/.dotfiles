aba2c8c93fa02110f0b7ad1899bc5c61f3ffdd9c
Thang Do <csessh@hey.com>
2025-04-14 21:07:18 +0700

    Readd dashboard

---
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -0,0 +1,27 @@
+return {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    dependencies = {
+        { "juansalvatore/git-dashboard-nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    opts = function()
+        local git_dashboard = require("git-dashboard-nvim").setup {
+            title = "owner_with_repo_name",
+            filled_squares = { "", "", "", "", "", "" },
+        }
+
+        local opts = {
+            theme = "doom",
+            config = {
+                header = git_dashboard,
+                center = {
+                    { action = "", desc = "", icon = "", key = "n" },
+                },
+                footer = function()
+                    return {}
+                end,
+            },
+        }
+        return opts
+    end,
+}
