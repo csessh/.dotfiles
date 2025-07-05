7ecae3ecf7b4bca3e0843266be08b4098afc8a8c
Thang Do <csessh@hey.com>
2025-07-05 15:34:35 +0700

    remap glance

--- nvim/.config/nvim/lua/plugins/glance.lua
+++ nvim/.config/nvim/lua/plugins/glance.lua
@@ -1,9 +1,9 @@
 return {
     "dnlhc/glance.nvim",
     keys = {
-        { "gd", ":Glance definitions<CR>", desc = "Glance at definition", silent = true },
-        { "gre", ":Glance references<CR>", desc = "Glance at references", silent = true },
-        { "gi", ":Glance implementations<CR>", desc = "Glance at implementations", silent = true },
+        { "<leader>gd", ":Glance definitions<CR>", desc = "Glance at definition", silent = true },
+        { "<leader>gr", ":Glance references<CR>", desc = "Glance at references", silent = true },
+        { "<leader>gi", ":Glance implementations<CR>", desc = "Glance at implementations", silent = true },
     },
     opts = {
         list = { position = "left" },
