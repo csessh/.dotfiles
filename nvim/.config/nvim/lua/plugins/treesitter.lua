fb0ed5f680068b30d783a45ef5203f7b5b60def5
Thang Do <csessh@hey.com>
2025-11-10 22:39:57 +0700

    treesitter-textobjects is a life saver

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,61 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require("nvim-treesitter.configs").setup {
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = true,
-            },
-            indent = { enable = true },
-        }
-    end,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            ---@diagnostic disable-next-line: missing-fields
+            require("nvim-treesitter.configs").setup {
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = true,
+                },
+                indent = { enable = true },
+            }
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            ---@diagnostic disable-next-line: missing-fields
+            require("nvim-treesitter.configs").setup {
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                        },
+                        include_surrounding_whitespace = false,
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+                        goto_next_start = {
+                            ["]f"] = "@function.outer",
+                            ["]c"] = { query = "@class.outer", desc = "Next class start" },
+                            ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+                        },
+                        goto_next_end = {
+                            ["]F"] = "@function.outer",
+                            ["]C"] = "@class.outer",
+                        },
+                        goto_previous_start = {
+                            ["[f"] = "@function.outer",
+                            ["[c"] = "@class.outer",
+                        },
+                        goto_previous_end = {
+                            ["[F"] = "@function.outer",
+                            ["[C"] = "@class.outer",
+                        },
+                    },
+                },
+            }
+        end,
+    },
 }
