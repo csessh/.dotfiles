8100f388e0c80753f0e91213d59330399f6de959
Thang Do <csessh@hey.com>
2025-12-02 09:20:13 +0700

    it's December. It's AOC time.

--- nvim/.config/nvim/lua/plugins/aoc.lua
+++ nvim/.config/nvim/lua/plugins/aoc.lua
@@ -1,8 +1,9 @@
 return {
   "csessh/aoc.nvim",
+  enabled = true,
   cond = string.find(vim.fn.getcwd(), vim.fn.expand "~/Documents/AoC/"),
   opts = {
-    session_filepath = "/var/tmp/aoc.txt",
+    session_filepath = "/var/tmp/aoc/session.txt",
     puzzle_input = {
       filename = "puzzle.txt",
       save_to_current_dir = true,
