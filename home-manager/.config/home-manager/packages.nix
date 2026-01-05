0739bf674ae3c5ae378c0c73e16044853ba9490b
Thang Do <tdo@hjkl.vn>
2026-01-05 22:48:59 +0700

    pin version for some essential packages

--- home-manager/.config/home-manager/packages.nix
+++ home-manager/.config/home-manager/packages.nix
@@ -1,6 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
+  assertions = [
+    {
+      assertion = builtins.compareVersions pkgs.fzf.version "0.67" >= 0;
+      message = "fzf must be 0.67+, got ${pkgs.fzf.version}";
+    }
+    {
+      assertion = builtins.compareVersions pkgs.neovim.version "0.11" >= 0;
+      message = "neovim must be 0.11+, got ${pkgs.neovim.version}";
+    }
+    {
+      assertion = builtins.compareVersions pkgs.lua5_4.version "5.4" >= 0;
+      message = "lua must be 5.4+, got ${pkgs.lua5_4.version}";
+    }
+  ];
+
   home.packages = with pkgs; [
     ############################
     # CLI tools
@@ -33,6 +48,7 @@
     # Languages & toolchains
     ############################
     go
+    lua5_4
     nodejs
     python3
     uv
